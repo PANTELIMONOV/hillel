8e862fd38cc903eaecc53baabaf98e41eeb3325e
mpantelimonov <pantelimonovmm@gmail.com>
2021-11-12 13:50:18 +0200

    Homework#2 updated task 4 and 5

--- src/org/project/hw2/Task4.java
+++ src/org/project/hw2/Task4.java
@@ -3,26 +3,24 @@ package org.project.hw2;
 import java.util.Scanner;
 
 public class Task4 {
-    private static final float coefficient = (float) 3.6;
-
-    public static float speed(float distance, float hours, float minutes, float seconds) {
-        return distance / (hours * 3600 + minutes * 60 + seconds);
-    }
-
     public static void main(String[] args) {
+        float coefficient = (float) 3.6;
+
         Scanner in = new Scanner(System.in);
 
         System.out.println("Please enter your distance:");
-        int distance = in.nextInt();
+        double distance = in.nextDouble();
         System.out.println("Please enter your hours:");
-        int hours = in.nextInt();
+        double hours = in.nextDouble();
         System.out.println("Please enter your minutes:");
-        int minutes = in.nextInt();
+        double minutes = in.nextDouble();
         System.out.println("Please enter your seconds:");
-        int seconds = in.nextInt();
+        double seconds = in.nextDouble();
 
-        System.out.println("Your speed in m/s: " + speed(distance, hours, minutes, seconds));
-        System.out.println("Your speed in km/h: " + speed(distance, hours, minutes, seconds) * coefficient);
+        double speed = distance / (hours * 3600 + minutes * 60 + seconds);
+
+        System.out.println("Your speed in m/s: " + speed);
+        System.out.println("Your speed in km/h: " + speed * coefficient);
     }
 }
 
--- src/org/project/hw2/Task5.java
+++ src/org/project/hw2/Task5.java
@@ -3,7 +3,6 @@ package org.project.hw2;
 import java.util.Scanner;
 
 public class Task5 {
-
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
